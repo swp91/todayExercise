64f3857cefe3b42e98c5b0980e46f202bab3fef4
swp91 <seongwoo4477@gmail.com>
2023-12-12 12:40:37 +0900

    fix:api 환경변수 롤백

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "https://api.todayex.site",
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/api/, ""), // "/api"를 제거함
-      },
-    },
-  },
 });
