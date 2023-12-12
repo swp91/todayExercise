ad63fdc48ba9169b5f2fcf85b884fffe67601ba4
swp91 <seongwoo4477@gmail.com>
2023-12-12 11:57:01 +0900

    fix: https api 수정

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://54.180.26.89:8080",
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, ""), // "/api"를 제거함
+      },
+    },
+  },
 });
