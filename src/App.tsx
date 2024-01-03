63dbc69f102ef484dcdee4c737d3269ca8b1609b
swp91 <seongwoo4477@gmail.com>
2024-01-04 00:15:48 +0900

    fix: 새로고침시 메인데이터 복구

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,12 @@ import { useEffect } from "react";
 import { LoginCheck } from "./api/UserApi";
 import { isLoggedInState } from "./recoil/User";
 import { useSetRecoilState } from "recoil";
+import { exericiseWeekRecord } from "./api/ExerciseApi";
+import { weekrecordsState } from "./recoil/ExerciseRecords";
 
 function App() {
   const setLoggedIn = useSetRecoilState(isLoggedInState);
+  const setRecords = useSetRecoilState(weekrecordsState);
 
   useEffect(() => {
     const checkSession = async () => {
@@ -13,6 +16,8 @@ function App() {
         const response = await LoginCheck();
         if (response.data.status === 200) {
           setLoggedIn(true);
+          const res = await exericiseWeekRecord();
+          setRecords(res.data.data);
         } else {
           setLoggedIn(false);
         }
